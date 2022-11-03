2cc5773f86e4d8fa2a142e59292ff83d908bf0e5
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-11-03 23:15:08 +0200

    fonts importations and configs

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, VStack, Center, Text } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIng';
+import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900" >
-        <Text color="white" fontSize={24}>Hello react native</Text>
-        <StatusBar style="auto" />
-      </Center>
+      {
+        fontsLoaded ? <SignIn /> : <Loading />
+      }
     </NativeBaseProvider>
   );
 }
